d59dafe96139c9ee8a2b06265caf7eb583b43ae0
rldunnam <rick@rickdunnam.com>
2026-02-20 16:35:24 -0600

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -6,14 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(jsx|tsx)$/,
-      exclude: /node_modules/,
-      use: ['@builder.io/nextjs-plugin-jsx-loc/loader'],
-    })
-    return config
-  },
 }
 
 module.exports = nextConfig
